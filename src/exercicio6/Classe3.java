e332b8cf021e8427bc55795e9545903dd512c1e4
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-25 16:31:13 -0300

    Exercicio 10 - Visibilidades e Pacotes

---
+++ src/exercicio6/Classe3.java
@@ -0,0 +1,15 @@
+package exercicio6;
+
+public class Classe3 {
+
+	Classe1 classe1;
+
+	void metodo() {
+
+		// atributo2 e 3
+
+		// metodo2 e 3
+
+	}
+
+}
---
+++ src/exercicio6/outro/pacote/Classe4.java
@@ -0,0 +1,15 @@
+package exercicio6.outro.pacote;
+
+import exercicio6.Classe1;
+
+public class Classe4 {
+
+	Classe1 classe1;
+
+	void metodo() {
+
+		// atributo3
+
+		// metodo3
+	}
+}
